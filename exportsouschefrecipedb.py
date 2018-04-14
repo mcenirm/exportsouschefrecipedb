5666e994f53e2eef245ca64ab069c5a39ac082ca
Michael McEniry [UAH] <mcenirm@uah.edu>
2018-04-14 16:10:36 -0500

    Add arguments

--- exportsouschefrecipedb.py
+++ exportsouschefrecipedb.py
@@ -2,12 +2,22 @@ import argparse
 import os
 
 
+def export_souschef_recipedb(path_to_recipedb_file, path_to_output_folder):
+    raise NotImplementedError()
+
+
 def main(argv, out, err):
     ap = argparse.ArgumentParser(
         prog=argv[0],
     )
-    ap.print_usage(file=err)
-    return os.EX_USAGE
+    ap.add_argument('recipedbfile')
+    ap.add_argument('outputfolder')
+    args = ap.parse_args(args=argv[1:])
+    export_souschef_recipedb(
+        path_to_recipedb_file=args.recipedbfile,
+        path_to_output_folder=args.outputfolder,
+    )
+    return os.EX_OK
 
 
 if __name__ == '__main__':
